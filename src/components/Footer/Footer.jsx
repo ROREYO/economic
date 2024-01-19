7c089a149209bb6064298a438869950cf861f571
REYO <rodrigoreychannel@gmail.com>
2024-01-19 19:49:20 +0300

    Добавление проекта

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,19 +9,19 @@ import InstagramSvg from '../../img/footer/instagram.svg';
 export const Footer = () => {
   return (
     <div className="footer">
-      <a href="https://vk.com/sotnya_rek" target="_blank">
+      <a href="https://vk.com/sotnya_rek" target="_blank" rel="noreferrer">
         <img src={VkSvg} alt="VK" />
       </a>
-      <a href="https://github.com/ROREYO" target="_blank">
+      <a href="https://github.com/ROREYO" target="_blank" rel="noreferrer">
         <img src={GithubSvg} alt="GitHub" />
       </a>
-      <a href="https://t.me/rey_0o0" target="_blank">
+      <a href="https://t.me/rey_0o0" target="_blank" rel="noreferrer">
         <img src={TelegramSvg} alt="Telegram" />
       </a>
-      <a href="https://www.linkedin.com/in/dmitry-dyak-a44b07253/" target="_blank">
+      <a href="https://www.linkedin.com/in/dmitry-dyak-a44b07253/" target="_blank" rel="noreferrer">
         <img src={LinkedinSvg} alt="LinkedIn" />
       </a>
-      <a href="https://www.instagram.com/sotnya_rek/" target="_blank">
+      <a href="https://www.instagram.com/sotnya_rek/" target="_blank" rel="noreferrer">
         <img src={InstagramSvg} alt="instagram" />
       </a>
     </div>
